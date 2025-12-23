cb035876f4b05ff8abcc61844df088a5b8f96316
colobrod72 <ftx1192@gmail.com>
2025-12-23 11:25:29 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,14 @@ RED='\033[0;31m'
 NOCOLOR='\033[0m'
 echo "${YELLOW}Введите пароль ${NOCOLOR}и нажмите ${RED}Enter!"
 sleep 1s
-echo "${YELLOW}Незакрывайте идёт обновление системы!!"
+echo "${YELLOW}Незакрывайте идёт установка пакетов!!"
+sudo apt -y install lsof rsync ecryptfs-utils cryptsetup
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install linux-headers-$(dpkg --print-architecture)
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install lightdm mousepad connman connman-gtk thunar terminology gparted neowofetch openbox obconf obsession tint2 compton lxappearance lxrandr bleachbit synaptic xxkb conky-std nitrogen
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt-get -f install
