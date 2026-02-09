1b3fb5d0dabf170402752bf3fe30c754414f3b11
colobrod72 <ftx1192@gmail.com>
2026-02-09 06:43:58 +0000

    Delete zx.sh

--- zx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt autoclean -y
-sudo apt autoremove -y
-sudo apt install -y xserver-xorg xinit lightdm lxrandr lxpolkit terminology lxterminal polkitd linux-headers-$(dpkg --print-architecture) nvidia-kernel-dkms nvidia-driver firmware-misc-nonfree thunar pluma geany geany-common rofi dunst feh lxappearance network-manager-applet redshift thunar thunar-archive-plugin thunar-volman pavucontrol pulsemixer pamixer pipewire-pulse lua-language-server picom compton fonts-recommended fonts-font-awesome fonts-terminus synaptic gparted pkexec mousepad bleachbit obsession nitrogen network-manager-gnome
