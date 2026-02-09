ef40cd03d99218cfa10b5dd94fc4a8898448245a
colobrod72 <ftx1192@gmail.com>
2026-02-09 06:18:28 +0000

    Delete zx.sh

--- zx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt autoremove -y
-sudo apt install -y xserver-xorg xinit lightdm lxrandr lxpolkit terminology lxterminal polkitd linux-headers-$(dpkg --print-architecture) nvidia-kernel-dkms nvidia-driver firmware-misc-nonfree thunar pluma geany geany-common
