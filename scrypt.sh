428daa96b7183cdeb1edb931b0eca84a798e8eab
colobrod72 <ftx1192@gmail.com>
2026-02-10 14:07:18 +0000

    Add files via upload

---
+++ scrypt.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+echo "${YELLOW}Введите пароль ${NOCOLOR}и нажмите ${RED}Enter!"
+echo "${YELLOW}Скрипт начинает работу ${NOCOLOR}хорошего дня ${RED}Вперёд!"
+sleep 3s
+echo "${YELLOW}Незакрывайте идёт установка пакетов!!"
+sudo cp debian.list /etc/apt/sources.list.d/
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt-get -f install
+sudo apt -y autoclean 
+sudo apt -y autoremove
+echo "${YELLOW}Операция обновления и очистки завершена!!"
+echo "${NOCOLOR}Всё хокей работаем дальше!!"
+sleep 2s
+sudo apt install -y  linux-headers-$(dpkg --print-architecture) awesome xorg-server xorg-xinit xorg-xbacklight xbindkeys xxkb xorg-xinput base-devel xdotool libnotify rofi dunst feh lxappearance network-manager-applet redshift thunar thunar-archive-plugin thunar-volman gvfs dialog mtools cifs-utils unzip pavucontrol pulsemixer pamixer pipewire-pulse acpi acpid flameshot qimgv xdg-user-dirs-gtk suckless-tools eza ttf-font-awesome terminus-font cmake meson curl pkgconf awesome-extra awesome-doc gparted synaptic nitrogen geany geany-common bleachbit lxrandr terminology lxterminal compton neowofetch mousepad lightdm grub-customizer cmus
+echo "${YELLOW}Пакеты загрузились ${NOCOLOR}Работаем дальше ${RED}Хокей приятной работы!!"
+sleep 4s
+cp -r /home/zx/devuan-excalibur/awesome /home/zx/.config
+echo "${YELLOW}Перезагружаемся!!"
+sleep 2s
+
