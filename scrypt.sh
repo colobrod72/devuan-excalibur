9fa0d0f9d9430e605316623995406f2e3d93668a
colobrod72 <ftx1192@gmail.com>
2026-02-10 22:13:15 +0000

    Update scrypt.sh

--- scrypt.sh
+++ scrypt.sh
@@ -15,7 +15,7 @@ sudo apt -y autoremove
 echo "${YELLOW}Операция обновления и очистки завершена!!"
 echo "${NOCOLOR}Всё хокей работаем дальше!!"
 sleep 2s
-sudo apt install -y  linux-headers-$(dpkg --print-architecture) awesome xserver-xorg xinit lightdm lxrandr lxpolkit polkitd pkexec network-manager-gnome xbindkeys xxkb xdotool libnotify rofi dunst feh lxappearance network-manager-applet redshift thunar thunar-archive-plugin thunar-volman gvfs dialog mtools cifs-utils unzip pavucontrol pulsemixer pamixer pipewire-pulse acpi acpid flameshot qimgv xdg-user-dirs-gtk suckless-tools eza ttf-font-awesome terminus-font cmake meson curl pkgconf awesome-extra awesome-doc gparted synaptic nitrogen geany geany-common bleachbit lxrandr terminology lxterminal compton neowofetch mousepad lightdm grub-customizer cmus
+sudo apt install -y linux-headers-$(dpkg --print-architecture) awesome xserver-xorg xinit lightdm lxrandr lxpolkit polkitd pkexec network-manager-gnome xbindkeys xxkb xdotool libnotify rofi dunst feh lxappearance network-manager-applet redshift thunar thunar-archive-plugin thunar-volman gvfs dialog mtools cifs-utils unzip pavucontrol pulsemixer pamixer pipewire-pulse acpi acpid flameshot qimgv xdg-user-dirs-gtk suckless-tools eza ttf-font-awesome terminus-font cmake meson curl pkgconf awesome-extra awesome-doc gparted synaptic nitrogen geany geany-common bleachbit lxrandr terminology lxterminal compton neowofetch mousepad lightdm grub-customizer cmus
 echo "${YELLOW}Пакеты загрузились ${NOCOLOR}Работаем дальше ${RED}Хокей приятной работы!!"
 sleep 4s
 cp -r /home/zx/devuan-excalibur/awesome /home/zx/.config
