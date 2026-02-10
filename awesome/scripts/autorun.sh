f597fdd98cc24c9d62648e3f0cf52819ecd9b19f
colobrod72 <ftx1192@gmail.com>
2026-02-10 06:53:09 +0000

    Delete awesome directory

--- awesome/scripts/autorun.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-lxpolkit &
-dunst -config ~/.config/awesome/dunst/dunstrc &
-picom --config ~/.config/awesome/picom/picom.conf --animations -b &
-feh --bg-fill ~/.config/awesome/wallpaper/wallhaven-ex63rk_3440x1440.png &
