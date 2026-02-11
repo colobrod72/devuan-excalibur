c969688a12cc8f1e1e7b9bedb34a3ed57ac10324
colobrod72 <ftx1192@gmail.com>
2026-02-11 08:35:18 +0000

    Update scrypt.sh

--- scrypt.sh
+++ scrypt.sh
@@ -8,15 +8,9 @@ sleep 3s
 echo "${YELLOW}Незакрывайте идёт установка пакетов!!"
 sudo cp debian.list /etc/apt/sources.list.d/
 sudo apt -y update
-sudo apt -y upgrade
-sudo apt-get -f install
-sudo apt -y autoclean 
-sudo apt -y autoremove
 echo "${YELLOW}Операция обновления и очистки завершена!!"
 echo "${NOCOLOR}Всё хокей работаем дальше!!"
 sleep 2s
-sudo nano /etc/sudoers
-/home/zx/devuan-excalibur/./scrypt.sh
 sudo apt install -y linux-headers-$(dpkg --print-architecture) awesome xserver-xorg xinit lightdm lxrandr lxpolkit polkitd pkexec network-manager-gnome xbindkeys xxkb xdotool rofi dunst feh lxappearance network-manager-applet redshift thunar thunar-archive-plugin thunar-volman gvfs dialog mtools cifs-utils unzip pavucontrol pulsemixer pamixer pipewire-pulse acpi acpid flameshot qimgv xdg-user-dirs-gtk suckless-tools eza curl pkgconf awesome-extra awesome-doc gparted synaptic nitrogen geany geany-common bleachbit lxrandr terminology lxterminal compton neowofetch mousepad grub-customizer cmus
 echo "${YELLOW}Пакеты загрузились ${NOCOLOR}Работаем дальше ${RED}Хокей приятной работы!!"
 sleep 4s
