e3051609add4c4d23a7b9a8f3be92d6399412c2d
colobrod72 <ftx1192@gmail.com>
2026-02-12 00:33:40 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,4 @@ sleep 2s
 sudo apt install -y  linux-headers-$(dpkg --print-architecture) awesome xserver-xorg xinit lxrandr lxpolkit polkitd pkexec network-manager-gnome xxkb rofi dunst feh redshift lxappearance network-manager-applet thunar thunar-archive-plugin thunar-volman pavucontrol pulsemixer pamixer pipewire-pulse flameshot xdg-user-dirs-gtk pkgconf awesome-extra awesome-doc gparted synaptic nitrogen geany geany-common bleachbit lxrandr terminology lxterminal neowofetch mousepad lightdm grub-customizer
 echo "${YELLOW}Грузим дальше!!"
 sleep 2s
-cp -r /home/zx/devuan-excalibur/awesome /home/zx/.config
-echo "${YELLOW}Пакеты загрузились ${NOCOLOR}Хокей ${RED}Приятной работы!! ${YELLOW}Перезагружаемся!!"
-sleep 3s
+
